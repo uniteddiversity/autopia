b6a4301274ada28c2167f0d2ac4ae798552a2d5a
postscript07@gmail.com <postscript07@gmail.com>
2017-01-13 12:20:55 +0000

    .

--- models/group.rb
+++ models/group.rb
@@ -30,50 +30,50 @@ class Group
     Account.where(:id.in => memberships.pluck(:account_id))
   end
   
-  after_create do
-    [  
-      'Admin, payment and grants',
-      'Transport and strike',
-      'Food',
-      'Toilets',
-      'Circles/community',
-      'Consent',
-      'Psychedelic welfare',
-      'Power, Privilege & Inclusion',
-      'Photography',
-      'Leave No Trace'
-    ].each { |team_name|
-      teams.create(name: team_name)
-    }
-    food = rotas.create(name: 'Food/kitchen')
-    [
-      'Kitchen lead',
-      'Kitchen 2',
-      'Kitchen 3',
-      'Kitchen 4',
-      'Kitchen 5',
-      'Kitchen 6',
-      'Wash up 1',
-      'Wash up 2',
-      'Wash up 3',
-      'Wash up 4',
-    ].each { |rota_role_name|
-      food.rota_roles.create name: rota_role_name
-    }
-    [
-      'Thurs dinner',
-      'Fri breakfast',
-      'Fri lunch',
-      'Fri dinner',
-      'Sat breakfast',
-      'Sat lunch',
-      'Sat dinner',
-      'Sun breakfast',
-      'Sun lunch'
-    ].each { |slot_name|
-      food.slots.create name: slot_name
-    }    
-  end
+#  after_create do
+#    [  
+#      'Admin, payment and grants',
+#      'Transport and strike',
+#      'Food',
+#      'Toilets',
+#      'Circles/community',
+#      'Consent',
+#      'Psychedelic welfare',
+#      'Power, Privilege & Inclusion',
+#      'Photography',
+#      'Leave No Trace'
+#    ].each { |team_name|
+#      teams.create(name: team_name)
+#    }
+#    food = rotas.create(name: 'Food/kitchen')
+#    [
+#      'Kitchen lead',
+#      'Kitchen 2',
+#      'Kitchen 3',
+#      'Kitchen 4',
+#      'Kitchen 5',
+#      'Kitchen 6',
+#      'Wash up 1',
+#      'Wash up 2',
+#      'Wash up 3',
+#      'Wash up 4',
+#    ].each { |rota_role_name|
+#      food.rota_roles.create name: rota_role_name
+#    }
+#    [
+#      'Thurs dinner',
+#      'Fri breakfast',
+#      'Fri lunch',
+#      'Fri dinner',
+#      'Sat breakfast',
+#      'Sat lunch',
+#      'Sat dinner',
+#      'Sun breakfast',
+#      'Sun lunch'
+#    ].each { |slot_name|
+#      food.slots.create name: slot_name
+#    }    
+#  end
         
   def self.admin_fields
     {
