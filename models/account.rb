6e553e9dfd99d85594c0400b3c0e92d3b006dd5d
postscript07@gmail.com <postscript07@gmail.com>
2017-01-18 16:11:46 +0000

    .

--- models/account.rb
+++ models/account.rb
@@ -80,87 +80,91 @@ class Account
     self.new_tips
   end
   
-def self.genders
-  [''] + %w{Nonbinary Woman Man}
-end  
+  def self.genders
+    [''] + %w{Nonbinary Woman Man}
+  end  
   
-def gender_symbol
-  case gender
-  when 'Man'
-    '<i title="Man" class="fa fa-mars"></i>'
-  when 'Woman'
-    '<i title="Woman" class="fa fa-venus"></i>'
-  when 'Nonbinary'
-    '<i title="Nonbinary" class="fa fa-transgender"></i>'
+  def self.gender_symbol(gender, pluralize: false)
+    case gender
+    when 'Man'
+      %Q{<i title="#{'Man'.pluralize}" class="fa fa-mars"></i>}
+    when 'Woman'
+      %Q{<i title="#{'Woman'.pluralize}" class="fa fa-venus"></i>}
+    when 'Nonbinary'
+      '<i title="Nonbinary" class="fa fa-transgender"></i>'
+    end
   end
-end
-  
-def age    
-  if dob = date_of_birth
-    now = Time.now.utc.to_date
-    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+
+  def gender_symbol
+    Account.gender_symbol(gender)
   end
-end  
   
-def self.human_attribute_name(attr, options={})  
-  {
-    :poc => 'I identify as a person of colour'
-  }[attr.to_sym] || super  
-end   
+  def age    
+    if dob = date_of_birth
+      now = Time.now.utc.to_date
+      now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+    end
+  end  
   
-def firstname
-  name.split(' ').first
-end
+  def self.human_attribute_name(attr, options={})  
+    {
+      :poc => 'I identify as a person of colour'
+    }[attr.to_sym] || super  
+  end   
+  
+  def firstname
+    name.split(' ').first
+  end
                
-def self.time_zones
-  ['']+ActiveSupport::TimeZone::MAPPING.keys.sort
-end  
+  def self.time_zones
+    ['']+ActiveSupport::TimeZone::MAPPING.keys.sort
+  end  
       
-def uid
-  id
-end
-  
-def info
-  {:email => email, :name => name}
-end
-  
-def self.authenticate(email, password)
-  account = find_by(email: /^#{Regexp.escape(email)}$/i) if email.present?
-  account && account.has_password?(password) ? account : nil
-end
-  
-before_save :encrypt_password, :if => :password_required
-
-def has_password?(password)
-  ::BCrypt::Password.new(crypted_password) == password
-end
-
-def self.generate_password(len)
-  chars = ("a".."z").to_a + ("0".."9").to_a
-  return Array.new(len) { chars[rand(chars.size)] }.join
-end   
-  
-def reset_password!
-  self.password = Account.generate_password(8)
-  if self.save
-    mail = Mail.new
-    mail.to = self.email
-    mail.from = "Huddl <team@huddl.tech>"
-    mail.subject = "New password for Huddl"
-    mail.body = "Hi #{self.firstname},\n\nSomeone (hopefully you) requested a new password on Huddl.\n\nYour new password is: #{self.password}\n\nYou can sign in at http://#{ENV['DOMAIN']}/accounts/sign_in."
-    mail.deliver       
-  else
-    return false
+  def uid
+    id
   end
-end
-
-private
   
-def encrypt_password
-  self.crypted_password = ::BCrypt::Password.create(self.password)
-end
+  def info
+    {:email => email, :name => name}
+  end
+  
+  def self.authenticate(email, password)
+    account = find_by(email: /^#{Regexp.escape(email)}$/i) if email.present?
+    account && account.has_password?(password) ? account : nil
+  end
+  
+  before_save :encrypt_password, :if => :password_required
 
-def password_required
-  crypted_password.blank? || self.password.present?
-end  
+  def has_password?(password)
+    ::BCrypt::Password.new(crypted_password) == password
+  end
+
+  def self.generate_password(len)
+    chars = ("a".."z").to_a + ("0".."9").to_a
+    return Array.new(len) { chars[rand(chars.size)] }.join
+  end   
+  
+  def reset_password!
+    self.password = Account.generate_password(8)
+    if self.save
+      mail = Mail.new
+      mail.to = self.email
+      mail.from = "Huddl <team@huddl.tech>"
+      mail.subject = "New password for Huddl"
+      mail.body = "Hi #{self.firstname},\n\nSomeone (hopefully you) requested a new password on Huddl.\n\nYour new password is: #{self.password}\n\nYou can sign in at http://#{ENV['DOMAIN']}/accounts/sign_in."
+      mail.deliver       
+    else
+      return false
+    end
+  end
+
+  private
+  
+  def encrypt_password
+    self.crypted_password = ::BCrypt::Password.create(self.password)
+  end
+
+  def password_required
+    crypted_password.blank? || self.password.present?
+  end  
 end
