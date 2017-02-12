16d2ee5ede3bf0828dc14cd858a7dbc142e46ef3
Stephen Reid <postscript07@gmail.com>
2017-02-12 22:45:02 +0000

    .

--- app/app.rb
+++ app/app.rb
@@ -381,14 +381,24 @@ module ActivateApp
       group_admins_only!
       @rslot.destroy
       redirect back      
-    end       
+    end      
+    
+    get '/rota/rslot/role/:rota_id/:rslot_id/:role_id' do
+      @rota = Rota.find(params[:rota_id]) || not_found 
+      @rslot = Rslot.find(params[:rslot_id]) || not_found 
+      @role = Role.find(params[:role_id]) || not_found 
+      @group = @rota.group
+      @membership = @group.memberships.find_by(account: current_account)
+      membership_required!      
+      partial :rota_rslot_role, :locals => {:rota => @rota, :rslot => @rslot, :role => @role}
+    end
          
     get '/shifts/create' do
       @rota = Rota.find(params[:rota_id]) || not_found 
       @group = @rota.group
       membership_required!
       Shift.create!(account: (params[:na] ? nil : current_account), rota_id: params[:rota_id], rslot_id: params[:rslot_id], role_id: params[:role_id])
-      redirect back
+      200
     end      
     
     get '/shifts/:id/destroy' do
@@ -397,7 +407,7 @@ module ActivateApp
       @membership = @group.memberships.find_by(account: current_account)
       halt unless (@shift.account and @shift.account.id == current_account.id) or @membership.admin?
       @shift.destroy
-      redirect back
+      200
     end    
     
     
--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,36 +1,6 @@
 
 $(function () {
 
-  $(document).on('submit', '[data-pagelet-url] form', function () {
-    var form = this
-    var pagelet = $(form).closest('[data-pagelet-url]')
-    pagelet.css('opacity', '0.1')
-    $.post($(form).attr('action'), $(form).serialize(), function () {
-      pagelet.load(pagelet.attr('data-pagelet-url'), function () {
-        pagelet.css('opacity', '1')
-      })
-    })
-    return false
-  })
-
-  $(document).on('click', '[data-pagelet-url] a.pagelet-trigger', function () {
-    var a = this
-    var pagelet = $(a).closest('[data-pagelet-url]')
-    pagelet.css('opacity', '0.1')
-    $.get($(a).attr('href'), function () {
-      pagelet.load(pagelet.attr('data-pagelet-url'), function () {
-        pagelet.css('opacity', '1')
-      })
-    })
-    return false
-  })
-
-  $('[data-pagelet-url]').each(function () {
-    var pagelet = this;
-    if ($(pagelet).html().length == 0)
-      $(pagelet).load($(pagelet).attr('data-pagelet-url'))
-  })
-
   $('[data-upload-url]').click(function () {
     var form = $('<form action="' + $(this).attr('data-upload-url') + '" method="post" enctype="multipart/form-data"><input style="display: none" type="file" name="upload"></form>')
     form.insertAfter(this)
@@ -97,5 +67,35 @@ $(function () {
       $(textarea).val($(summernote).code());
     });
   });
+  
+  $(document).on('submit', '[data-pagelet-url] form', function () {
+    var form = this
+    var pagelet = $(form).closest('[data-pagelet-url]')
+    pagelet.css('opacity', '0.1')
+    $.post($(form).attr('action'), $(form).serialize(), function () {
+      pagelet.load(pagelet.attr('data-pagelet-url'), function () {
+        pagelet.css('opacity', '1')
+      })
+    })
+    return false
+  })
+
+  $(document).on('click', '[data-pagelet-url] a.pagelet-trigger', function () {        
+    var a = this
+    var pagelet = $(a).closest('[data-pagelet-url]')    
+    pagelet.css('opacity', '0.1')
+    $.get($(a).attr('href'), function () {
+      pagelet.load(pagelet.attr('data-pagelet-url'), function () {
+        pagelet.css('opacity', '1')
+      })
+    })
+    return false
+  })
+
+  $('[data-pagelet-url]').each(function () {
+    var pagelet = this;
+    if ($(pagelet).html().length == 0)
+      $(pagelet).load($(pagelet).attr('data-pagelet-url'))
+  })  
 
 });
