e1c0af619ef8276c5ad7c10e98556c1a592a6819
postscript07@gmail.com <postscript07@gmail.com>
2017-02-21 14:22:06 +0000

    .

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -9,7 +9,7 @@ $(function () {
     })
   })
 
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip({html: true})
 
   if ($('label[for=account_poc').length > 0)
     $('label[for=account_poc').html($('label[for=account_poc').html().replace('person of colour', '<a target="_blank" href="https://en.wikipedia.org/wiki/Person_of_color">person of colour</a>'))
