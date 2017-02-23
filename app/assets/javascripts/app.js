c7d0de463b6ded736d126a6e8d9a7f911e7f1e61
Stephen Reid <postscript07@gmail.com>
2017-02-23 22:11:40 +0000

    .

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -11,7 +11,7 @@ $(function () {
     })
   })
 
-  $('[data-toggle="tooltip"]').tooltip({html: true})
+  $('[data-toggle="tooltip"]').tooltip({html: true, viewport: false})
 
   if ($('label[for=account_poc').length > 0)
     $('label[for=account_poc').html($('label[for=account_poc').html().replace('person of colour', '<a target="_blank" href="https://en.wikipedia.org/wiki/Person_of_color">person of colour</a>'))
