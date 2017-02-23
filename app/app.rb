a59ab5e80b5ee50ee4a60986227c923bd739526a
postscript07@gmail.com <postscript07@gmail.com>
2017-02-23 15:39:19 +0000

    .

--- app/app.rb
+++ app/app.rb
@@ -142,7 +142,7 @@ module ActivateApp
       @group = Group.find_by(slug: params[:slug]) || not_found
       @membership = @group.memberships.find_by(account: current_account)
       membership_required!
-      @notifications = @group.notifications.order('created_at desc')
+      @notifications = @group.notifications.order('created_at desc').page(params[:page])
       erb :newsfeed
     end
             
