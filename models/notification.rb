2f2df52f85a60acf5c0d79f850c3a2ad1f631102
postscript07@gmail.com <postscript07@gmail.com>
2017-02-26 13:27:39 +0000

    .

--- models/notification.rb
+++ models/notification.rb
@@ -46,10 +46,10 @@ class Notification
     </style>
   </head>
   <body>
-    <h2 style="margin-top: 0"><a style="text-decoration: none" href="http://#{ENV['DOMAIN']}/h/#{group.slug}">#{group.name}</a></h2>
+    <h1 style="margin-top: 0"><a style="text-decoration: none" href="http://#{ENV['DOMAIN']}/h/#{group.slug}">#{group.name}</a></h1>
     <p>#{notification.sentence}. <a href="#{notification.link[1]}">#{notification.link[0]}</a></p>
     <p>Best,<br />Team Huddl</p>
-    <p style="font-size: 12px;"><a style="color: #aaa" href="http://#{ENV['DOMAIN']}/accounts/edit">Stop these emails</a></p>
+    <p style="font-size: 12px;"><a style="color: #aaa !important" href="http://#{ENV['DOMAIN']}/accounts/edit">Visit your profile to stop these emails</a></p>
   </body>
 </html>
           }
