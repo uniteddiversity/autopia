a0990bb9fc4743062288e7e29704e506b76c1289
postscript07@gmail.com <postscript07@gmail.com>
2017-03-19 16:50:26 +0000

    .

--- app/app.rb
+++ app/app.rb
@@ -58,6 +58,10 @@ module Huddl
       erb :not_found, :layout => :application
     end
     
+    get '/raise' do
+      raise Time.to_i.to_s
+    end    
+    
     get '/' do
       erb :home
     end
