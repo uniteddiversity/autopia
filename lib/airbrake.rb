299726d99d8f9053eb0eed74d8415bfdbf4985a4
postscript07@gmail.com <postscript07@gmail.com>
2017-03-19 15:46:47 +0000

    .

--- lib/airbrake.rb
+++ lib/airbrake.rb
@@ -1,5 +1,6 @@
-Airbrake.configure do |config|
-  config.api_key = ENV['AIRBRAKE_API_KEY']
-  config.host = ENV['AIRBRAKE_HOST'] if ENV['AIRBRAKE_HOST']
-  config.ignore << "Sinatra::NotFound"
+if ENV['AIRBRAKE_PROJECT_ID'] or ENV['AIRBRAKE_API_KEY']
+  Airbrake.configure do |config|
+    config.project_id = ENV['AIRBRAKE_PROJECT_ID'] or ENV['AIRBRAKE_API_KEY']
+    config.project_key = ENV['AIRBRAKE_PROJECT_KEY'] or ENV['AIRBRAKE_API_KEY']
+  end
 end
