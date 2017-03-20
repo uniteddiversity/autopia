d5330ca445d07fc746fc3df01d9d17f22faefb7f
Stephen Reid <postscript07@gmail.com>
2017-03-20 10:22:40 +0000

    .

--- app/app.rb
+++ app/app.rb
@@ -28,7 +28,7 @@ module Huddl
         :user_name => ENV['SMTP_USERNAME'],
         :password => ENV['SMTP_PASSWORD'],
         :address => ENV['SMTP_ADDRESS'],
-        :port => 587
+        :port => 587  
       } 
     end
        
