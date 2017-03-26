967f5d637de63c0e8328a9d70a1111fad826c165
Stephen Reid <postscript07@gmail.com>
2017-03-26 11:09:14 +0000

    .

--- models/notification.rb
+++ models/notification.rb
@@ -18,7 +18,7 @@ class Notification
   end
   
   def self.mailable_types
-    %w{applied joined_group listed_activity created_transport created_tier created_team created_accom created_rota listed_spend created_timetable commented}
+    %w{applied joined_group listed_activity created_transport created_team created_rota listed_spend created_timetable commented}
   end
   
   after_create do
