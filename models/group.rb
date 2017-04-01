719464012818f5203cb7d6cdcc29f351ec64691c
Stephen Reid <postscript07@gmail.com>
2017-04-01 16:26:40 +0000

    .

--- models/group.rb
+++ models/group.rb
@@ -44,9 +44,10 @@ class Group
       mail.from = "Huddl <notifications@huddl.tech>"
       mail.subject = "New group: #{name}"
       
+      group = self
       html_part = Mail::Part.new do
         content_type 'text/html; charset=UTF-8'
-        body %Q{#{account.name} (#{account.email}) created a new group: <a href="http://#{ENV['DOMAIN']}/h/#{slug}">#{name}</a>"}
+        body %Q{#{group.account.name} (#{group.account.email}) created a new group: <a href="http://#{ENV['DOMAIN']}/h/#{group.slug}">#{group.name}</a>}
       end
       mail.html_part = html_part
       
