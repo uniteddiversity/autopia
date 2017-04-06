232861f54726da907ca5be73ffcae06f4ab801af
Stephen Reid <postscript07@gmail.com>
2017-04-06 08:11:54 +0000

    .

--- models/membership.rb
+++ models/membership.rb
@@ -26,14 +26,14 @@ class Membership
   
   has_many :notifications, as: :notifiable, dependent: :destroy
   after_create do
-    
-    if general = group.teams.find_by(name: 'General')
-      general.teamships.create! account: account
-    end
-    
+        
     if mapplication or added_by
       notifications.create! :group => group, :type => 'joined_group'
     end
+    
+    if general = group.teams.find_by(name: 'General')
+      general.teamships.create! account: account
+    end    
     
     if ENV['SMTP_ADDRESS']
       mail = Mail.new
