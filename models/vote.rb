3c9f6e6e12fdf7a0a9d362aabf1f6f062acf9534
Stephen Reid <postscript07@gmail.com>
2017-04-25 09:28:14 +0000

    .

--- models/vote.rb
+++ models/vote.rb
@@ -5,6 +5,8 @@ class Vote
   belongs_to :option, index: true
   belongs_to :account, index: true
   
+  validates_uniqueness_of :account, :scope => :option
+  
   def self.admin_fields
     {
       :option_id => :lookup,
