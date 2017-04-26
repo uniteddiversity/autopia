5000a94ed793da18daf759c35b0783a666be716f
Stephen Reid <postscript07@gmail.com>
2017-04-26 10:16:10 +0000

    fixes https://github.com/wordsandwriting/huddl/issues/35

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -47,8 +47,8 @@ $(function () {
   tooltip();
   autosize($('textarea'));
 
-  if ($('label[for=account_poc').length > 0)
-    $('label[for=account_poc').html($('label[for=account_poc').html().replace('person of colour', '<a target="_blank" href="https://en.wikipedia.org/wiki/Person_of_color">person of colour</a>'))
+  if ($('label[for=account_poc]').length > 0)
+    $('label[for=account_poc]').html($('label[for=account_poc]').html().replace('person of colour', '<a target="_blank" href="https://en.wikipedia.org/wiki/Person_of_color">person of colour</a>'))
 
   $('input[type=text].slug').each(function () {
     var slug = $(this);
