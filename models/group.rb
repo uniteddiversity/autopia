6b2e161fd2f3ae761e8be4cc406b762421c23848
Stephen Reid <postscript07@gmail.com>
2017-05-18 21:18:46 +0000

    .

--- models/group.rb
+++ models/group.rb
@@ -90,7 +90,7 @@ class Group
   end
   after_save :replace_route
   def replace_route
-    if @_replace_route
+    if persisted? and @_replace_route
       @_replace_route = nil
       delete_route
       create_route      
