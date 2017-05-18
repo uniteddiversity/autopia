50ab87798ffe4c1df00d2b2082b6b11f0c717249
Stephen Reid <postscript07@gmail.com>
2017-05-18 15:22:58 +0000

    .

--- lib/incoming_mailgun.rb
+++ lib/incoming_mailgun.rb
@@ -1,8 +1,6 @@
 class EmailReceiver < Incoming::Strategies::Mailgun
 	setup :api_key => ENV['MAILGUN_API_KEY']
-	
   def receive(mail)
-    raise params.inspect
+    raise request.inspect
   end
-  
 end
