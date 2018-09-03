d7e1792c9cd3ab5d182fce908417c0741ccc5a79
Stephen Reid <stephen@stephenreid.net>
2018-09-03 16:45:07 +0100

    .

--- models/habit_completion_like.rb
+++ models/habit_completion_like.rb
@@ -22,19 +22,21 @@ class HabitCompletionLike
       habit_completion = habit_completion_like.habit_completion
       habit = habit_completion.habit
 
-      mail = Mail.new
-      mail.to = habit_completion.account.email
-      mail.from = ENV['NOTIFICATION_EMAIL']
-      mail.subject = "#{habit_completion_like.account.name} liked your completion of #{habit.name}"
+      unless habit_completion.account.unsubscribed?
+        mail = Mail.new
+        mail.to = habit_completion.account.email
+        mail.from = ENV['NOTIFICATION_EMAIL']
+        mail.subject = "#{habit_completion_like.account.name} liked your completion of #{habit.name}"
             
-      content = ERB.new(File.read(Padrino.root('app/views/emails/habit_completion_like.erb'))).result(binding)
-      html_part = Mail::Part.new do
-        content_type 'text/html; charset=UTF-8'
-        body ERB.new(File.read(Padrino.root('app/views/layouts/email.erb'))).result(binding)
-      end
-      mail.html_part = html_part
+        content = ERB.new(File.read(Padrino.root('app/views/emails/habit_completion_like.erb'))).result(binding)
+        html_part = Mail::Part.new do
+          content_type 'text/html; charset=UTF-8'
+          body ERB.new(File.read(Padrino.root('app/views/layouts/email.erb'))).result(binding)
+        end
+        mail.html_part = html_part
       
-      mail.deliver
+        mail.deliver
+      end
     end    
   end
   handle_asynchronously :send_like
