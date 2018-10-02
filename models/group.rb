291361a86e6b17bb618fa04a7367d6279b818118
Stephen Reid <stephen@stephenreid.net>
2018-10-02 22:28:32 +0100

    .

--- models/group.rb
+++ models/group.rb
@@ -151,7 +151,7 @@ class Group
   end  
   
   def vouchers
-    enable_supporters ? 'proposers + supporters' : 'proposers'
+    enable_supporters ? 'proposers + supporters (with at least one proposer)' : 'proposers'
   end
   
   def incomings
