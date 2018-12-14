48d5e2a485e8bdefaaa74f37bd8c1d3e2365ffa3
Stephen Reid <stephen@stephenreid.net>
2018-12-14 23:48:48 +0000

    .

--- app/controllers/vibes.rb
+++ app/controllers/vibes.rb
@@ -1,17 +1,23 @@
 Autopo::App.controller do
   
-  get '/vibe/:id' do
+  get '/vibes/:id' do
+    sign_in_required!
+    @account = Account.find(params[:id]) || not_found
+    partial :vibe, :locals => {:account => @account}
+  end
+  
+  post '/vibe/:id' do
     sign_in_required!
     @account = Account.find(params[:id]) || not_found
     current_account.vibes_as_viber.create vibee: @account
-    redirect back
+    200
   end
   
-  get '/unvibe/:id' do
+  post '/unvibe/:id' do
     sign_in_required!
     @account = Account.find(params[:id]) || not_found
     current_account.vibes_as_viber.find_by(vibee: @account).destroy
-    redirect back
+    200
   end  
   
 end
