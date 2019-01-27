410616b2ac78c9bca0e1818ba683f300db75a120
Stephen Reid <stephen@stephenreid.net>
2019-01-27 21:39:36 +1300

    .

--- models/message.rb
+++ models/message.rb
@@ -26,4 +26,28 @@ class Message
     !read?(messanger, messangee)
   end
   
+  after_create :send_email  
+  def send_email
+    if ENV['SMTP_ADDRESS'] and !messangee.unsubscribed?
+      message = self
+      messanger = message.messanger      
+      messangee = message.messangee
+      
+      mail = Mail.new
+      mail.to = messangee.email
+      mail.from = "#{messanger.name} <#{messanger.email}>"
+      mail.subject = "Message from #{messanger.name} via Autopo"
+            
+      content = ERB.new(File.read(Padrino.root('app/views/emails/message.erb'))).result(binding)
+      html_part = Mail::Part.new do
+        content_type 'text/html; charset=UTF-8'
+        body ERB.new(File.read(Padrino.root('app/views/layouts/email.erb'))).result(binding)
+      end
+      mail.html_part = html_part
+      
+      mail.deliver
+    end    
+  end
+  handle_asynchronously :send_email   
+  
 end
