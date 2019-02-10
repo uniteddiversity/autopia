3bfb6f892ca5a5f1def7d9b537158db254bfedbf
Stephen Reid <stephen@stephenreid.net>
2019-02-10 19:42:52 +1300

    .

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -125,37 +125,3 @@ $(function () {
   });
 
 });
-
-
-const grantPermission = () => {
-  if (!('Notification' in window)) {
-    alert('This browser does not support system notifications');
-    return;
-  }
-
-  if (Notification.permission === 'granted') {
-    new Notification('You are already subscribed to web notifications');
-    return;
-  }
-
-  if (
-          Notification.permission !== 'denied' ||
-          Notification.permission === 'default'
-          ) {
-    Notification.requestPermission().then(result => {
-      if (result === 'granted') {
-        const notification = new Notification(
-                'Awesome! You will start receiving notifications shortly'
-                );
-      }
-    });
-  }
-};
-grantPermission();
-
-const showNotification = data => {
-  const title = `${data}`
-  new Notification(title);
-};
-
-showNotification('Welcome to Autopo!');
