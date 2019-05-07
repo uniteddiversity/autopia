515eccfc933027f745c049b4edf0621d62d05198
Stephen Reid <stephen@stephenreid.net>
2019-05-07 12:55:30 +0100

    .

--- app/controllers/features.rb
+++ app/controllers/features.rb
@@ -1,6 +1,7 @@
 Autopia::App.controller do
 
   get '/features/:id' do
+    sign_in_required!
     @feature = Feature.find(params[:id]) || not_found
     request.xhr? ? partial(:'features/feature', :object => @feature) : erb(:'features/feature')
   end
