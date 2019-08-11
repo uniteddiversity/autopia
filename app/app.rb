9a535b4e413fd4b8f2583f90f38d4945f9553b41
Stephen Reid <stephen@stephenreid.net>
2019-08-11 14:51:09 +0100

    .

--- app/app.rb
+++ app/app.rb
@@ -49,7 +49,7 @@ module Autopia
         account.update_attribute(:sign_in_token, SecureRandom.uuid)
       end      
       @_params = params; def params; @_params; end # force controllers to inherit the fixed params
-      @og_desc = 'Autopia is the social network for people co-creating the more beautiful world our hearts know is possible'
+      @og_desc = 'Autopia is a social network emerging from co-created microfestivals and other real-life gatherings'
       @og_image = "#{ENV['BASE_URI']}/images/autopia-link.png"
       if current_account
         current_account.set(last_active: Time.now)
