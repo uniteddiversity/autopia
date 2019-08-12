806ddc5ca768b1e8edf41be9f18678cb4371a85e
Stephen Reid <stephen@stephenreid.net>
2019-08-12 12:56:05 +0100

    .

--- models/notification.rb
+++ models/notification.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 class Notification
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :type, :type => String
-  
+  field :type, type: String
+
   belongs_to :circle, polymorphic: true, index: true
   belongs_to :notifiable, polymorphic: true, index: true
-  
+
   def self.circle_types
-    %w{Gathering Account}
+    %w[Gathering Account]
   end
-  
+
   def circle_url
     if circle.is_a?(Gathering)
       "#{ENV['BASE_URI']}/a/#{circle.slug}"
@@ -18,52 +20,52 @@ class Notification
       "#{ENV['BASE_URI']}/accounts/#{circle.id}"
     end
   end
-  
+
   validates_presence_of :type
-    
+
   before_validation do
     errors.add(:type, 'not found') unless Notification.types.include?(type)
   end
-  
+
   def self.types
-    %w{created_gathering applied joined_gathering created_team created_timetable created_activity created_rota created_tier created_accom created_transport created_spend created_room created_place updated_profile updated_place followed completed_a_habit liked_a_habit_completion joined_team signed_up_to_a_shift interested_in_activity scheduled_activity unscheduled_activity made_admin unadmined cultivating_quality commented reacted_to_a_comment left_gathering created_payment created_inventory_item mapplication_removed}
+    %w[created_gathering applied joined_gathering created_team created_timetable created_activity created_rota created_tier created_accom created_transport created_spend created_room created_place updated_profile updated_place followed completed_a_habit liked_a_habit_completion joined_team signed_up_to_a_shift interested_in_activity scheduled_activity unscheduled_activity made_admin unadmined cultivating_quality commented reacted_to_a_comment left_gathering created_payment created_inventory_item mapplication_removed]
   end
-  
+
   def self.mailable_types
-    %w{created_gathering applied joined_gathering created_team created_timetable created_activity created_rota created_tier created_accom created_transport created_spend created_room created_place}
+    %w[created_gathering applied joined_gathering created_team created_timetable created_activity created_rota created_tier created_accom created_transport created_spend created_room created_place]
   end
-    
-  after_create :send_email  
+
+  after_create :send_email
   def send_email
     if ENV['SMTP_ADDRESS'] && Notification.mailable_types.include?(type)
-      notification = self      
+      notification = self
       circle = self.circle
       bcc = circle.emails
-      
-      if bcc.length > 0
+
+      unless bcc.empty?
         mail = Mail.new
         mail.bcc = bcc
         mail.from = ENV['NOTIFICATION_EMAIL']
         mail.subject = "[#{circle.name}] #{Nokogiri::HTML(notification.sentence).text}"
-            
+
         content = ERB.new(File.read(Padrino.root('app/views/emails/notification.erb'))).result(binding)
         html_part = Mail::Part.new do
           content_type 'text/html; charset=UTF-8'
           body ERB.new(File.read(Padrino.root('app/views/layouts/email.erb'))).result(binding)
         end
         mail.html_part = html_part
-      
+
         mail.deliver
       end
-    end    
+    end
   end
-  handle_asynchronously :send_email 
-      
-  def sentence    
+  handle_asynchronously :send_email
+
+  def sentence
     case type.to_sym
     when :created_gathering
       gathering = notifiable
-      "<strong>#{gathering.account.name}</strong> created the gathering"    
+      "<strong>#{gathering.account.name}</strong> created the gathering"
     when :applied
       mapplication = notifiable
       "<strong>#{mapplication.account.name}</strong> applied"
@@ -89,7 +91,7 @@ class Notification
       "<strong>#{follow.follower.name}</strong> followed <strong>#{follow.followee.name}</strong>"
     when :completed_a_habit
       habit_completion = notifiable
-      "<strong>#{habit_completion.account.name}</strong> completed the habit <strong>#{habit_completion.habit.name}</strong>"      
+      "<strong>#{habit_completion.account.name}</strong> completed the habit <strong>#{habit_completion.habit.name}</strong>"
     when :liked_a_habit_completion
       habit_completion_like = notifiable
       "<strong>#{habit_completion_like.account.name}</strong> liked <strong>#{habit_completion_like.habit.account.name}</strong>'s completion of <strong>#{habit_completion_like.habit.name}</strong>"
@@ -107,7 +109,7 @@ class Notification
       "<strong>#{account.name}</strong> updated their profile"
     when :updated_place
       place = notifiable
-      "<strong>#{place.name}</strong> was updated"      
+      "<strong>#{place.name}</strong> was updated"
     when :created_activity
       activity = notifiable
       "<strong>#{activity.account.name}</strong> proposed the activity <strong>#{activity.name}</strong> under <strong>#{activity.timetable.name}</strong>"
@@ -122,16 +124,16 @@ class Notification
       "<strong>#{transport.account.name}</strong> created the transport <strong>#{transport.name}</strong>"
     when :created_tier
       tier = notifiable
-      "<strong>#{tier.account.name}</strong> created the tier <strong>#{tier.name}</strong>"      
+      "<strong>#{tier.account.name}</strong> created the tier <strong>#{tier.name}</strong>"
     when :created_team
       team = notifiable
-      "<strong>#{team.account.name}</strong> created the team <strong>#{team.name}</strong>"            
+      "<strong>#{team.account.name}</strong> created the team <strong>#{team.name}</strong>"
     when :created_accom
       accom = notifiable
-      "<strong>#{accom.account.name}</strong> created the accommodation <strong>#{accom.name}</strong>"                  
+      "<strong>#{accom.account.name}</strong> created the accommodation <strong>#{accom.name}</strong>"
     when :created_rota
       rota = notifiable
-      "<strong>#{rota.account.name}</strong> created the rota <strong>#{rota.name}</strong>"                        
+      "<strong>#{rota.account.name}</strong> created the rota <strong>#{rota.name}</strong>"
     when :scheduled_activity
       activity = notifiable
       "<strong>#{activity.scheduled_by.name}</strong> scheduled the activity <strong>#{activity.name}</strong>"
@@ -143,34 +145,40 @@ class Notification
       "<strong>#{membership.account.name}</strong> was made an admin by <strong>#{membership.admin_status_changed_by.name}</strong>"
     when :unadmined
       membership = notifiable
-      "<strong>#{membership.account.name}</strong> was unadmined by <strong>#{membership.admin_status_changed_by.name}</strong>"      
+      "<strong>#{membership.account.name}</strong> was unadmined by <strong>#{membership.admin_status_changed_by.name}</strong>"
     when :created_timetable
       timetable = notifiable
-      "<strong>#{timetable.account.name}</strong> created the timetable <strong>#{timetable.name}</strong>"      
+      "<strong>#{timetable.account.name}</strong> created the timetable <strong>#{timetable.name}</strong>"
     when :cultivating_quality
       cultivation = notifiable
-      "<strong>#{cultivation.account.name}</strong> is cultivating <strong>#{cultivation.quality.name}</strong>"                  
+      "<strong>#{cultivation.account.name}</strong> is cultivating <strong>#{cultivation.quality.name}</strong>"
     when :commented
       comment = notifiable
       if comment.commentable.is_a?(Mapplication)
-        "<strong>#{comment.account.name}</strong> commented on <strong>#{comment.commentable.account.name}</strong>'s application"                  
+        "<strong>#{comment.account.name}</strong> commented on <strong>#{comment.commentable.account.name}</strong>'s application"
+      elsif comment.commentable.is_a?(Habit)
+        if comment.first_in_post?
+          "<strong>#{comment.account.name}</strong> started a thread <strong>#{comment.commentable.account.name}/#{comment.commentable.name}#{if comment.post.subject; "/#{comment.post.subject}"; end}</strong>"
+        else
+          "<strong>#{comment.account.name}</strong> replied to <strong>#{comment.commentable.account.name}/#{comment.commentable.name}#{if comment.post.subject; "/#{comment.post.subject}"; end}</strong>"
+        end
       elsif comment.commentable.is_a?(Account)
         if comment.first_in_post?
-          "<strong>#{comment.account.name}</strong> started a thread <strong>#{if comment.post.subject; "#{comment.post.subject}"; end}</strong>"
+          "<strong>#{comment.account.name}</strong> started a thread <strong>#{comment.post.subject&.to_s}</strong>"
         else
-          "<strong>#{comment.account.name}</strong> replied to <strong>#{if comment.post.subject; "#{comment.post.subject}"; end}</strong>"
-        end          
+          "<strong>#{comment.account.name}</strong> replied to <strong>#{comment.post.subject&.to_s}</strong>"
+        end
       else
         if comment.first_in_post?
-          "<strong>#{comment.account.name}</strong> started a thread <strong>#{comment.commentable.name}#{if comment.post.subject; "/#{comment.post.subject}"; end}</strong>"                  
+          "<strong>#{comment.account.name}</strong> started a thread <strong>#{comment.commentable.name}#{if comment.post.subject; "/#{comment.post.subject}"; end}</strong>"
         else
-          "<strong>#{comment.account.name}</strong> replied to <strong>#{comment.commentable.name}#{if comment.post.subject; "/#{comment.post.subject}"; end}</strong>"                  
+          "<strong>#{comment.account.name}</strong> replied to <strong>#{comment.commentable.name}#{if comment.post.subject; "/#{comment.post.subject}"; end}</strong>"
         end
-      end      
+      end
     when :reacted_to_a_comment
       comment_reaction = notifiable
       if comment_reaction.commentable.is_a?(Account)
-        "<strong>#{comment_reaction.account.name}</strong> reacted with #{comment_reaction.body} to <strong>#{comment_reaction.comment.account.name}'s</strong> comment in <strong>#{if comment_reaction.comment.post.subject; "#{comment_reaction.comment.post.subject}"; end}</strong>"  
+        "<strong>#{comment_reaction.account.name}</strong> reacted with #{comment_reaction.body} to <strong>#{comment_reaction.comment.account.name}'s</strong> comment in <strong>#{comment_reaction.comment.post.subject&.to_s}</strong>"
       else
         "<strong>#{comment_reaction.account.name}</strong> reacted with #{comment_reaction.body} to <strong>#{comment_reaction.comment.account.name}'s</strong> comment in <strong>#{comment_reaction.commentable.name}#{if comment_reaction.comment.post.subject; "/#{comment_reaction.comment.post.subject}"; end}</strong>"
       end
@@ -188,7 +196,7 @@ class Notification
       "<strong>#{account.name}</strong>'s application was deleted"
     end
   end
-  
+
   def link
     case type.to_sym
     when :created_gathering
@@ -196,12 +204,12 @@ class Notification
     when :applied
       ['View applications', "#{ENV['BASE_URI']}/a/#{circle.slug}/applications"]
     when :joined_gathering
-      ['View members', "#{ENV['BASE_URI']}/a/#{circle.slug}/members"]      
+      ['View members', "#{ENV['BASE_URI']}/a/#{circle.slug}/members"]
     when :joined_team
       ['View team', "#{ENV['BASE_URI']}/a/#{circle.slug}/teams/#{notifiable.team_id}"]
     when :followed
       ['View profile', "#{ENV['BASE_URI']}/u/#{notifiable.followee.username}"]
-    when :completed_a_habit          
+    when :completed_a_habit
       ['View habit', "#{ENV['BASE_URI']}/habits/#{notifiable.habit.id}"]
     when :liked_a_habit_completion
       ['View habit', "#{ENV['BASE_URI']}/habits/#{notifiable.habit.id}"]
@@ -214,33 +222,33 @@ class Notification
     when :updated_profile
       ['View profile', "#{ENV['BASE_URI']}/u/#{notifiable.username}"]
     when :updated_place
-      ['View place', "#{ENV['BASE_URI']}/places/#{notifiable.id}"]      
+      ['View place', "#{ENV['BASE_URI']}/places/#{notifiable.id}"]
     when :created_activity
       ['View activity', "#{ENV['BASE_URI']}/a/#{circle.slug}/activities/#{notifiable.id}"]
     when :signed_up_to_a_shift
       ['View rotas', "#{ENV['BASE_URI']}/a/#{circle.slug}/rotas/#{notifiable.rota_id}"]
     when :interested_in_activity
-      ['View activity', "#{ENV['BASE_URI']}/a/#{circle.slug}/activities/#{notifiable.activity_id}"]  
+      ['View activity', "#{ENV['BASE_URI']}/a/#{circle.slug}/activities/#{notifiable.activity_id}"]
     when :created_transport
-      ['View transport', "#{ENV['BASE_URI']}/a/#{circle.slug}/transports"] 
+      ['View transport', "#{ENV['BASE_URI']}/a/#{circle.slug}/transports"]
     when :created_tier
-      ['View tiers', "#{ENV['BASE_URI']}/a/#{circle.slug}/tiers"]    
+      ['View tiers', "#{ENV['BASE_URI']}/a/#{circle.slug}/tiers"]
     when :created_team
       ['View team', "#{ENV['BASE_URI']}/a/#{circle.slug}/teams/#{notifiable.id}"]
     when :created_accom
-      ['View accommodation', "#{ENV['BASE_URI']}/a/#{circle.slug}/accoms"]      
+      ['View accommodation', "#{ENV['BASE_URI']}/a/#{circle.slug}/accoms"]
     when :created_rota
       ['View rotas', "#{ENV['BASE_URI']}/a/#{circle.slug}/rotas/#{notifiable.id}"]
     when :scheduled_activity
-      ['View activity', "#{ENV['BASE_URI']}/a/#{circle.slug}/activities/#{notifiable.id}"]  
+      ['View activity', "#{ENV['BASE_URI']}/a/#{circle.slug}/activities/#{notifiable.id}"]
     when :unscheduled_activity
-      ['View activity', "#{ENV['BASE_URI']}/a/#{circle.slug}/activities/#{notifiable.id}"]  
+      ['View activity', "#{ENV['BASE_URI']}/a/#{circle.slug}/activities/#{notifiable.id}"]
     when :made_admin
-      ['View members', "#{ENV['BASE_URI']}/a/#{circle.slug}/members"]      
+      ['View members', "#{ENV['BASE_URI']}/a/#{circle.slug}/members"]
     when :unadmined
-      ['View members', "#{ENV['BASE_URI']}/a/#{circle.slug}/members"]      
+      ['View members', "#{ENV['BASE_URI']}/a/#{circle.slug}/members"]
     when :created_timetable
-      ['View timetable', "#{ENV['BASE_URI']}/a/#{circle.slug}/timetables/#{notifiable.id}"]      
+      ['View timetable', "#{ENV['BASE_URI']}/a/#{circle.slug}/timetables/#{notifiable.id}"]
     when :cultivating_quality
       ['View qualities', "#{ENV['BASE_URI']}/a/#{circle.slug}/qualities"]
     when :commented
@@ -257,7 +265,7 @@ class Notification
       ['View applications', "#{ENV['BASE_URI']}/a/#{circle.slug}/applications"]
     end
   end
-    
+
   def icon
     case type.to_sym
     when :created_gathering
@@ -265,13 +273,13 @@ class Notification
     when :applied
       'fa-file-text-o'
     when :joined_gathering
-      'fa-user-plus'      
+      'fa-user-plus'
     when :joined_team
       'fa-group'
     when :followed
       'fa-user-plus'
     when :completed_a_habit
-      'fa-check'      
+      'fa-check'
     when :liked_a_habit_completion
       'fa-thumbs-up'
     when :created_spend
@@ -283,7 +291,7 @@ class Notification
     when :updated_profile
       'fa-user-circle-o'
     when :updated_place
-      'fa-map-marker'      
+      'fa-map-marker'
     when :created_activity
       'fa-paper-plane'
     when :signed_up_to_a_shift
@@ -309,33 +317,31 @@ class Notification
     when :unadmined
       'fa-key'
     when :created_timetable
-      'fa-table'   
+      'fa-table'
     when :cultivating_quality
-      'fa-star'  
+      'fa-star'
     when :commented
-      'fa-comment'       
+      'fa-comment'
     when :reacted_to_a_comment
-      'fa-thumbs-up' 
+      'fa-thumbs-up'
     when :left_gathering
       'fa fa-sign-out'
     when :created_payment
-      'fa-money'  
+      'fa-money'
     when :created_inventory_item
       'fa-wrench'
     when :mapplication_removed
       'fa-file-text-o'
-    end    
+    end
   end
 
-        
   def self.admin_fields
     {
-      :circle_type => :text,
-      :circle_id => :text,
-      :notifiable_type => :text,
-      :notifiable_id => :text,
-      :type => :text
+      circle_type: :text,
+      circle_id: :text,
+      notifiable_type: :text,
+      notifiable_id: :text,
+      type: :text
     }
   end
-    
 end
