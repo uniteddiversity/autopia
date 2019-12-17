e459ec4707596d27641c618082eef101ce4b1b99
Stephen Reid <stephen@stephenreid.net>
2019-12-17 21:48:05 +0000

    .

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -15,25 +15,9 @@ Autopia::App.helpers do
     @current_account ||= Account.find(session[:account_id]) if session[:account_id]
   end
 
-  def admin?
-    current_account && current_account.admin?
-  end
-
-  def creator?(createable)
-    current_account and createable.account and createable.account.id == current_account.id
-  end  
-
   def random(relation, n)
     count = relation.count
     (0..count-1).sort_by{rand}.slice(0, n).collect! do |i| relation.skip(i).first end
-  end
-
-  def sign_in_required!
-    unless current_account
-      flash[:notice] = 'You must sign in to access that page'
-      session[:return_to] = request.url
-      request.xhr? ? halt : redirect('/accounts/sign_in')
-    end
   end
 
   def timeago(x)
@@ -50,63 +34,71 @@ Autopia::App.helpers do
     @feature = Feature.find_by(name: name) || Feature.create(name: name)
   end
   
+  
+    
+  def kick!
+    flash[:notice] = "You don't have access to that page"
+    session[:return_to] = request.url
+    request.xhr? ? halt(403) : redirect(account ? '/' : '/accounts/sign_in')          
+  end
+  
+  def creator?(createable, account=current_account)
+    account and createable.account and createable.account.id == account.id
+  end  
+  
+  def admin?(account=current_account)
+    account && account.admin?
+  end  
+  def admins_only!; kick! unless admin?; end
+  
   def organisation_admin?(organisation=nil, account=current_account)
     organisation = @organisation if !organisation
-    account && (organisation.account_id == account.id || organisation.organisationships.find_by(account: account, admin: true) || account.admin?)
-  end  
-  
-  def organisation_admins_only!(organisation=nil, account=current_account)
-    organisation = @organisation if !organisation
-    unless organisation_admin?(organisation, account)
-      flash[:notice] = 'You must be a team member of that organisation to access that page'
-      session[:return_to] = request.url
-      request.xhr? ? halt(403) : redirect(account ? '/' : '/accounts/sign_in')      
-    end
-  end
-  
+    account && organisation && (organisation.account_id == account.id || organisation.organisationships.find_by(account: account, admin: true) || account.admin?)
+  end    
+  def organisation_admins_only!; kick! unless organisation_admin?; end  
+    
   def activity_admin?(activity=nil, account=current_account)
     activity = @activity if !activity
-    activity.activityships.find_by(account: account, admin: true) || organisation_admin?(activity.organisation, account)
+    account && activity && activity.activityships.find_by(account: account, admin: true) || organisation_admin?(activity.organisation, account)
   end  
-  
-  def activity_admins_only!(activity=nil, account=current_account)
-    activity = @activity if !activity
-    unless activity_admin?(activity, account)
-      flash[:notice] = 'You must be an admin of that activity to access that page'
-      session[:return_to] = request.url
-      request.xhr? ? halt(403) : redirect(account ? '/' : '/accounts/sign_in')      
-    end    
-  end  
-  
-  
+  def activity_admins_only!; kick! unless activity_admin?; end  
+    
   def local_group_admin?(local_group=nil, account=current_account)
     local_group = @local_group if !local_group
-    local_group.local_groupships.find_by(account: account, admin: true) || organisation_admin?(local_group.organisation, account)
+    account && local_group && local_group.local_groupships.find_by(account: account, admin: true) || organisation_admin?(local_group.organisation, account)
   end  
+  def local_group_admins_only!; kick! unless local_group_admin?; end  
   
-  def local_group_admins_only!(local_group=nil, account=current_account)
-    local_group = @local_group if !local_group
-    unless local_group_admin?(local_group, account)
-      flash[:notice] = 'You must be an admin of that local group to access that page'
-      session[:return_to] = request.url
-      request.xhr? ? halt(403) : redirect(account ? '/' : '/accounts/sign_in')      
-    end    
+  def organisation_assistant?(organisation=nil, account=current_account)
+    organisation = @organisation if !organisation
+    account && organisation && organisation_admin?(organisation, account) or organisation.local_groups.any? { |local_group| local_group_admin?(local_group, account) } or organisation.activities.any? { |activity| activity_admin?(activity, account) }
   end  
-  
+  def organisation_assistants_only!; kick! unless organisation_assistant?; end  
+    
   def event_admin?(event=nil, account=current_account)
     event = @event if !event
-    account && (event.account_id == account.id || event.revenue_sharer_id == account.id || event.event_facilitations.find_by(account: account) || (event.activity && activity_admin?(event.activity, account)) || (event.organisation && organisation_admin?(event.organisation, account)) || account.admin?)
+    account && event && (event.account_id == account.id || event.revenue_sharer_id == account.id || event.event_facilitations.find_by(account: account) || (event.activity && activity_admin?(event.activity, account)) || (event.organisation && organisation_admin?(event.organisation, account)) || account.admin?)
   end  
+  def event_admins_only!; kick! unless event_admin?; end  
   
-  def event_admins_only!(event=nil, account=current_account)
-    event = @event if !event
-    unless event_admin?(event, account)
-      flash[:notice] = 'You must be an admin of that event to access that page'
-      session[:return_to] = request.url
-      request.xhr? ? halt(403) : redirect(account ? '/' : '/accounts/sign_in')      
-    end
+  def gathering_admin?(gathering=nil, account=current_account)
+    gathering = @gathering if !gathering
+    account && gathering and ((membership = gathering.memberships.find_by(account: account)) and membership.admin?)
   end
-
+  def gathering_admins_only!; kick! unless gathering_admin?; end  
+  
+  
+  
+  
+  
+  def sign_in_required!
+    unless current_account
+      flash[:notice] = 'You must sign in to access that page'
+      session[:return_to] = request.url
+      request.xhr? ? halt : redirect('/accounts/sign_in')
+    end
+  end  
+    
   def membership_required!(gathering=nil, account=current_account)
     gathering = @gathering if !gathering
     unless account and gathering and gathering.memberships.find_by(account: account)
@@ -129,22 +121,5 @@ Autopia::App.helpers do
       end
     end
   end
-
-  def admins_only!
-    unless current_account and current_account.admin?
-      flash[:notice] = 'You must be an admin to access that page'
-      session[:return_to] = request.url
-      request.xhr? ? halt(403) : redirect(current_account ? '/' : '/accounts/sign_in')
-    end
-  end
-
-  def gathering_admins_only!(gathering=nil)
-    gathering = @gathering if !gathering
-    unless current_account and gathering and ((membership = gathering.memberships.find_by(account: current_account)) and membership.admin?)
-      flash[:notice] = 'You must be an admin of that gathering to access that page'
-      session[:return_to] = request.url
-      request.xhr? ? halt(403) : redirect(current_account ? '/' : '/accounts/sign_in')
-    end
-  end
-
+  
 end
